c14cff782a5cfc575eb989e9dc2dd6baab15e876
Alexandre Moreno Ribeiro Magalhães <amrm@cin.ufpe.br>
2018-04-05 12:56:54 -0300

    D:

--- 2018.1/atividades/02-LL1parsing/src/br/ufpe/cin/if688/parsing/analysis/SetGenerator.java
+++ 2018.1/atividades/02-LL1parsing/src/br/ufpe/cin/if688/parsing/analysis/SetGenerator.java
@@ -6,6 +6,8 @@ import br.ufpe.cin.if688.parsing.grammar.*;
 
 
 public final class SetGenerator {
+	
+	//public static  talvez economizar codigo fazendo recursivamente quando o first for nonterminal
 
 	public static Map<Nonterminal, Set<GeneralSymbol>> getFirst(Grammar g) {
         
@@ -19,6 +21,7 @@ public final class SetGenerator {
     	//nonterminal, terminal, specialsymbol
     	Collection<Production> prod = g.getProductions();
     	Iterator<Production> x = prod.iterator();
+    	
     	while(x.hasNext()) {
     		Production a = x.next();
     		Set<GeneralSymbol> ss = first.get(a.getNonterminal());
@@ -30,7 +33,23 @@ public final class SetGenerator {
     			}else if(ls.get(i).getClass().getSimpleName().equalsIgnoreCase("specialsymbol")) {
     				ss.add(ls.get(i));
     				break;
+    			}else if(ls.get(i).getClass().getSimpleName().equalsIgnoreCase("nonterminal")) {
+    				Nonterminal aa = (Nonterminal) a.getProduction().get(i);
+    				//tenho que achar a produção desse não terminal
+    				boolean found = false;
+    				while(!found) {
+    					Iterator<Production> y = prod.iterator();
+    					while(y.hasNext()) {
+        					Production k = y.next();
+        					if(k.getNonterminal().equals(aa)) { //se ao percorrer novamente as produções acho o nao terminal, vejo o first do mesmo
+        						
+        					}
+        				}
+    				}
+    				
+    				
     			}
+    			System.out.println();
     		}
     		first.put(a.getNonterminal(), ss);
     		
